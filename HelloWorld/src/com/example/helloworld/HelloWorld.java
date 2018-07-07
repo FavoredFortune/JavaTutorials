173311eb651c124c084d5d1eb11ca72e38503075
Suzanne Richman <suzanne@favoredfortune.com>
2018-07-06 21:51:08 -0700

    first commit building and debugging app done

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+/**Following IntellJ IDEA tutorial
+ * https://github.com/FavoredFortune/JavaTutorials
+created by Suzanne Richman 2018
+
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("it's me, Wombat!");
+    }
+    }
